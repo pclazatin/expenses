44dd436346069b3523d07b589bad7bea7c05f2de
pclazatin <patricia.lazatin@gmail.com>
2023-07-08 12:36:17 +0800

    add invoke.py

--- moneymanager.py
+++ moneymanager.py
@@ -1,4 +1,4 @@
-"""" this script imports the export file from money manager app
+""" this script imports the export file from money manager app
 converts the raw file into the two expense reports
 
 1. main categories
@@ -21,9 +21,9 @@ import datetime as dt
 # variables
 # -------------------
 TABLES = {
-    'events':[]
+    'events': []
 }
-CSV_FILENAME = '2022.csv'
+CSV_FILENAME = '2023.csv'
 REPORTING_YEAR = 2020
 REPORTING_MONTH = 12
 
@@ -56,7 +56,7 @@ def update_config():
 
 
 # ------------------
-# subfunctions
+# sub-functions
 # ------------------
 def load_transactions():
     global TABLES
@@ -70,7 +70,7 @@ def load_transactions():
     events['day'] = events['start_date'].apply(lambda x: x.day)
     events['month'] = events['start_date'].apply(lambda x: x.month)
     events['year'] = events['start_date'].apply(lambda x: x.year)
-    events = events[events['Income/Expenses'] == 'Expenses'].copy()
+    events = events[events['Income/Expense'] == 'Expense'].copy()
     events = events[events['year'] <= REPORTING_YEAR].copy()
     events = events[events['month'] <= REPORTING_MONTH].copy()
     events_pvt = pd.pivot_table(events, index='Category', columns='month',
@@ -79,27 +79,28 @@ def load_transactions():
     TABLES['main_category_report'] = events_pvt
 
     sub_cat_pvt = pd.pivot_table(events, index=['Category', 'Subcategory'], columns='month',
-                             values='Amount', aggfunc='sum')
+                                 values='Amount', aggfunc='sum')
     sub_cat_pvt.fillna(0, inplace=True)
     TABLES['subcategory_report'] = sub_cat_pvt
 
+
 def post_to_gsheet():
-    #01 main category
+    # 01 main category
     main_category_report = TABLES['main_category_report']
-    #data fields
+    # data fields
     db.post_to_gsheet(main_category_report, 'expenses', 'main_category_report',
                       input_option='USER_ENTERED')
-    #category field
+    # category field
     db.post_to_gsheet(main_category_report.reset_index()[['Category']],
                       'expenses', 'main_categories',
                       input_option='USER_ENTERED')
 
-    #02 subcategories
+    # 02 subcategories
     subcategory_report = TABLES['subcategory_report']
-    #data fields
+    # data fields
     db.post_to_gsheet(subcategory_report, 'expenses', 'subcategory_report',
                       input_option='USER_ENTERED')
-    #category field
+    # category field
     db.post_to_gsheet(subcategory_report.reset_index()[['Category', 'Subcategory']],
                       'expenses', 'subcategories',
                       input_option='USER_ENTERED')
