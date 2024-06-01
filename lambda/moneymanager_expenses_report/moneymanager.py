36b1fa72e74669edb6630659d2f195af9321b778
pclazatin <patricia.lazatin@gmail.com>
2024-06-01 16:10:58 +0800

    test

--- lambda/moneymanager_expenses_report/moneymanager.py
+++ lambda/moneymanager_expenses_report/moneymanager.py
@@ -6,7 +6,7 @@ tables:
 
 the report has rows of the categories
 and columns for each month in the year
-
+#test1
 """
 #-----------------------------------------------------------------------------
 #import dependencies
